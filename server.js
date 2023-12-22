13581d233da868fa381c413ca09ffb8459bd8229
nikita-pandey4456 <153998948+nikita-pandey4456@users.noreply.github.com>
2023-12-22 20:08:30 +0530

    defining models and schemas

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const axios = require('axios');
 
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors'); 
 
 const app = express();
 app.use(cors({ origin: '*' }));
